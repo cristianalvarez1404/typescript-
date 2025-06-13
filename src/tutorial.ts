ba7e4db510a6a68dc0def744cd187a5c5a4b34f4
CristianAlvarez <cristianandres1496@gmail.com>
2025-06-12 21:51:07 -0500

    typescript course

---
+++ src/tutorial.ts
@@ -0,0 +1,41 @@
+console.log("typescript tutorial");
+
+// interface someValue {
+//   id: number;
+//   name: string;
+// }
+
+// let someObj: someValue = {
+//   id: 1,
+//   name: "cristian",
+// };
+
+// console.log(someObj);
+
+let awesomeName: string = "shakeAndBake";
+awesomeName = "something";
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+
+//awesomeName = 20;
+
+let amount: number = 20;
+amount = 12 - 1;
+// amount = "pants";
+
+let isAwesome: boolean = true;
+isAwesome = false;
+// isAwesome = "shakeAndBake";
+
+let greetring: string = "Hello, Typescript!";
+greetring = greetring.toUpperCase();
+
+let age: number = 25;
+age = age + 5;
+
+let isAdult: boolean = age >= 18;
+// console.log(isAdult);
+
+// greetring = 10;
+// age = "thirty";
+// isAdult = "yes";
